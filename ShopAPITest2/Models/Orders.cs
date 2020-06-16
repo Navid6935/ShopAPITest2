8c22d58d9ec79679eb4b2b28948132c686a1e097
NaviD <NaviD@DESKTOP-ILMPI5P>
2020-06-17 01:06:42 +0430

    Add project files.

---
+++ ShopAPITest2/Models/Orders.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShopAPITest2.Models
+{
+    public partial class Orders
+    {
+        public int OrderId { get; set; }
+        public DateTime? Date { get; set; }
+        public double? Total { get; set; }
+        public string Status { get; set; }
+        public int CutomerId { get; set; }
+        public int SalesPersonId { get; set; }
+
+        public virtual SalesPerson SalesPerson { get; set; }
+    }
+}
---
+++ ShopAPITest2/Models/ShopApiDBContext.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ShopAPITest2.Models
+{
+    public partial class ShopApiDBContext : DbContext
+    {
+        public ShopApiDBContext()
+        {
+        }
+
+        public ShopApiDBContext(DbContextOptions<ShopApiDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customer { get; set; }
+        public virtual DbSet<Orders> Orders { get; set; }
+        public virtual DbSet<OrdersItem> OrdersItem { get; set; }
+        public virtual DbSet<Products> Products { get; set; }
+        public virtual DbSet<SalesPerson> SalesPerson { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.City).HasMaxLength(150);
+
+                entity.Property(e => e.Email).HasMaxLength(150);
+
+                entity.Property(e => e.FirstName).HasMaxLength(150);
+
+                entity.Property(e => e.LastName).HasMaxLength(150);
+
+                entity.Property(e => e.Phone).HasMaxLength(150);
+
+                entity.Property(e => e.State).HasMaxLength(150);
+
+                entity.Property(e => e.ZipCode).HasMaxLength(150);
+            });
+
+            modelBuilder.Entity<Orders>(entity =>
+            {
+                entity.HasKey(e => e.OrderId);
+
+                entity.Property(e => e.Date).HasColumnType("datetime");
+
+                entity.Property(e => e.Status).HasMaxLength(150);
+
+                entity.HasOne(d => d.SalesPerson)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.SalesPersonId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Orders_SalesPerson");
+            });
+
+            modelBuilder.Entity<OrdersItem>(entity =>
+            {
+                entity.HasKey(e => e.OrderItemId);
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrdersItem)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_OrdersItem_Products");
+            });
+
+            modelBuilder.Entity<Products>(entity =>
+            {
+                entity.HasKey(e => e.ProductId);
+
+                entity.Property(e => e.ProductName).HasMaxLength(150);
+
+                entity.Property(e => e.Status).HasMaxLength(150);
+
+                entity.Property(e => e.Variently).HasMaxLength(150);
+            });
+
+            modelBuilder.Entity<SalesPerson>(entity =>
+            {
+                entity.Property(e => e.Address).HasMaxLength(150);
+
+                entity.Property(e => e.City).HasMaxLength(150);
+
+                entity.Property(e => e.Email).HasMaxLength(150);
+
+                entity.Property(e => e.FirstName).HasMaxLength(150);
+
+                entity.Property(e => e.LastName).HasMaxLength(150);
+
+                entity.Property(e => e.Phone).HasMaxLength(150);
+
+                entity.Property(e => e.State).HasMaxLength(150);
+
+                entity.Property(e => e.ZipCode).HasMaxLength(150);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
